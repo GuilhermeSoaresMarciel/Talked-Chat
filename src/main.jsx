81aa13118c074e9b5252cd963784c7ff42cda8f1
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-09-09 14:21:22 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode } from "react";
+
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
@@ -7,18 +8,33 @@ import img_main from "/Talked Chat.png";
 
 import Message from "./components/Message";
 
+function defAPI() {
+  fetch("/TalkedChat/get")
+    .then((response) => response.json())
+    .then((data) => {
+      const chat = document.querySelector("#obj_chat");
+
+      chat.innerHTML = "";
+
+      data.forEach((message) => {
+        const element = document.createElement("div");
+
+        element.textContent = message;
+
+        chat.appendChild(element);
+      });
+    });
+}
+
 createRoot(document.querySelector("body")).render(
   <StrictMode>
     <header>
       <img src={img_main} />
-
       <h1 className="text-2xl font-extrabold">Talked Chat</h1>
     </header>
 
     <main>
-      <div className="obj_chat" id="obj_chat">
-        <Message m_text="Hello, I am Talked Chat, how are you?" />
-      </div>
+      <div className="obj_chat" id="obj_chat"></div>
 
       <div className="obj_input">
         <input type="text" placeholder="Write Here" id="textHere" />
@@ -40,3 +56,7 @@ createRoot(document.querySelector("body")).render(
     </footer>
   </StrictMode>,
 );
+
+setTimeout(() => {
+  defAPI();
+}, 0);
