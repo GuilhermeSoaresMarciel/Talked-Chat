c2a5ca4ff4cfce0b3bdb541b437417360038f5d4
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-09-09 14:21:26 -0300

    Update dataBase.js

--- src/api/dataBase.js
+++ src/api/dataBase.js
@@ -1,1 +1,11 @@
 import express from "express";
+
+const app = express();
+
+const list_chat = ["Olá", "Tudo bem?", "Como você está?"];
+
+app.get("/TalkedChat/get", (req, res) => {
+  res.json(list_chat);
+});
+
+export default app;
