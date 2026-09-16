0a835e750a4682b806e2a3969e987ca1aaf7611f
wllysses <wllyssestavares@gmail.com>
2026-09-16 09:48:30 -0300

    refactor

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,7 +1,31 @@
-function Message({ m_text }) {
+function Message({ messages, setMessages }) {
+  function handleDeleteMessageById(id) {
+    if (confirm("Deseja realmente apagar esta mensagem?")) {
+      const filteredMessages = messages.filter(
+        (_message, index) => index !== id,
+      );
+      setMessages(filteredMessages);
+      return;
+    }
+  }
+
   return (
-    <div className="Message">
-      <p>{m_text}</p>
+    <div className="text-center p-4 flex flex-col gap-2">
+      {!messages.length ? (
+        <p>No messages</p>
+      ) : (
+        messages.map((message, index) => (
+          <p key={index} className="bg-lime-300 rounded p-2 text-black">
+            {message}{" "}
+            <button
+              onClick={() => handleDeleteMessageById(index)}
+              className="bg-red-500 size-6 rounded-full text-white text-xs"
+            >
+              X
+            </button>
+          </p>
+        ))
+      )}
     </div>
   );
 }
